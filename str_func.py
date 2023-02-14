052c3971f35201e87faf597998805fc423f2c9e9
Shebelnitskii <Shebelnitskiywork@gmail.com>
2023-02-14 16:26:20 +0400

    adding a function str_title to str_func.py and adding docstring

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def str_upper():
+    """Функция принимающая текст и выводящая текст верхнем регистре"""
     user_input = input()
     print(user_input.upper())
+
+def str_title():
+    """Функция принимающая текст и выводящая первые буквы в верхнем регистре"""
+    user_input = input()
+    print(user_input.title())
